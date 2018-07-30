ad88069d087d94ee5d571d8dddea93966705e3de
JohnPark <jongmanpark418@gmail.com>
2018-07-29 19:13:30 -0700

    first commit

---
+++ src/main/java/com/ezbudget2/EZBudget/EzBudgetApplication.java
@@ -0,0 +1,12 @@
+package com.ezbudget2.EZBudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzBudgetApplication.class, args);
+	}
+}
