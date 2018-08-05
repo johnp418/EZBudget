379d7c70afe342ae9139c46fa43e72b13445555a
JohnPark <jongmanpark418@gmail.com>
2018-08-04 22:51:13 -0700

    - Added db schema document

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # EZBudget
+
+## Development
+Start mysql docker container
+```bash
+docker run --name EZBudget -e MYSQL_ROOT_PASSWORD=aaaa -d mysql:tag
+```
--- src/main/java/com/ezbudget2/EZBudget/EzBudgetApplication.java
+++ src/main/java/com/ezbudget2/EZBudget/EzBudgetApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EzBudgetApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EzBudgetApplication.class, args);
 	}
 }
